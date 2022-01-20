ea6c842a75a7c34e7545e8768893c474fad20a75
Silva <jrjrsilva@gmail.com>
2022-01-20 20:50:58 -0300

    Personalizando retorno validação

--- src/main/java/br/com/solucoescjm/mc/dto/CategoriaDTO.java
+++ src/main/java/br/com/solucoescjm/mc/dto/CategoriaDTO.java
@@ -14,8 +14,8 @@ public class CategoriaDTO implements Serializable {
 		
 		private Integer id;
 		
-		@NotEmpty(message="Preenchimento orbigatório")
-		@Length(min = 5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
+		@NotEmpty(message="Preenchimento obrigatório")
+		@Length(min = 5, max=80, message="O tamanho deve ter entre 5 e 80 caracteres")
 		private String nome;
 		
 		public CategoriaDTO() {
