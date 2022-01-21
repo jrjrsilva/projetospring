17c68d34641b698e9b1ec4e872c4dc8fa7874e45
Silva <jrjrsilva@gmail.com>
2022-01-20 22:27:35 -0300

    GET PUT DELETE Cliente

--- src/main/java/br/com/solucoescjm/mc/domain/Cliente.java
+++ src/main/java/br/com/solucoescjm/mc/domain/Cliente.java
@@ -65,7 +65,15 @@ public class Cliente implements Serializable{
 		this.nome = nome;
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
-		this.tipo = tipo.getCod();
+		this.tipo = (tipo == null) ? null : tipo.getCod();
+	}
+	
+	
+
+	public Cliente(Integer id, String nome, String email) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
 	}
 
 	public Integer getId() {
--- src/main/java/br/com/solucoescjm/mc/services/ClienteService.java
+++ src/main/java/br/com/solucoescjm/mc/services/ClienteService.java
@@ -1,12 +1,20 @@
 package br.com.solucoescjm.mc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.solucoescjm.mc.domain.Cliente;
+import br.com.solucoescjm.mc.domain.Cliente;
+import br.com.solucoescjm.mc.dto.ClienteDTO;
 import br.com.solucoescjm.mc.repositories.ClienteRepository;
+import br.com.solucoescjm.mc.services.exception.DataIntegrityException;
 import br.com.solucoescjm.mc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -19,5 +27,41 @@ public class ClienteService {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj,obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		// TODO Auto-generated method stub
+		newObj.setNome(obj.getNome());	
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		  }catch(DataIntegrityViolationException e ) { throw new
+		  DataIntegrityException("Não é possível excluir Cliente que tem pedidos");
+		  }
+		 
+	}
+	
+	public List<Cliente> findAll() {
+		return repo.findAll();
+		
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage,String orderBy, String direction ){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(),objDto.getEmail(), null, null);
 	}
 }
